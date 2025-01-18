f97a0b5eb8dd592d3bca959aac24a53c7c58ceba
Dominic-Khoo <dominickhooyx@gmail.com>
2025-01-18 17:12:23 +0800

    Fix merge conflicts

--- src/background.ts
+++ src/background.ts
@@ -5,21 +5,3 @@ chrome.runtime.onInstalled.addListener(async () => {
   const { data, error } = await supabase.from('my_table').select('*')
   console.log('Data:', data, 'Error:', error)
 })
-
-const ALLOWED_DOMAIN = "https://leetcode.com"
-
-chrome.webRequest.onBeforeRequest.addListener(
-  (details) => {
-    const url = details.url;
-
-    // If the request is NOT to the allowed domain, we redirect:
-    if (!url.startsWith(ALLOWED_DOMAIN)) {
-      return { redirectUrl: ALLOWED_DOMAIN };
-    }
-
-    // Otherwise, do nothing (allow the request).
-    return {};
-  },
-  { urls: ["<all_urls>"] }, // Listen for all URLs
-  ["blocking"] // We need the blocking option to redirect
-);
