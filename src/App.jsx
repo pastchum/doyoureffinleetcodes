be8fc963e05dd81d4132f14b712a931a395f45e6
pramoth20 <gmpramoth@gmail.com>
2025-01-18 15:09:26 +0800

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import leetcodeLogo from './icons/leetcode.png'
 import profileLogo from './icons/profile.png'
 import './App.css'
+import Dashboard from './dashboard/App' // Import the Dashboard component
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://leetcode.com" target="_blank">
-          <img src={leetcodeLogo} className="logo" alt="LeetCode logo" />
-        </a>
-      <a href="/dashboard" target="_self">
-        <img src={profileLogo} className="logo" alt="Profile logo" />
-      </a>
+        <Link to="https://leetcode.com" target="_blank">
+          <img src={leetcodeLogo} className="logo leetcode-logo" alt="LeetCode logo" />
+        </Link>
+        <Link to="/dashboard">
+          <img src={profileLogo} className="logo profile-logo" alt="Profile logo" />
+        </Link>
       </div>
+      <Routes>
+        <Route path="/" element={<Home count={count} setCount={setCount} />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  )
+}
+
+function Home({ count, setCount }) {
+  return (
+    <>
       <h1>Leetcode NOW</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
